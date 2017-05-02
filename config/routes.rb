8f2d250789a9a43210defce1236774560c8b2d14
Tamiko Terada <tamikodevo@gmail.com>
2017-05-02 15:52:56 -0700

    add route for recipes#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
+  get '/recipes', to: 'recipes#index', as: 'recipes'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
