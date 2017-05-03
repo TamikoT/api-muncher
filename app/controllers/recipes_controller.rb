e7d535bcc3e5b2ef406bd0955967df88f8b5e861
Tamiko Terada <tamikodevo@gmail.com>
2017-05-03 09:17:27 -0700

    reference new apiwrapper in search method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,30 +1,11 @@
-class RecipesController
-# < ApplicationController
-  require 'httparty'
-  require 'dotenv/load'
-  require 'awesome_print'
+require 'edamam_apiwrapper'
 
-  BASE_URL = "https://api.edamam.com/search\?"
-  EDAMAM_ID = ENV["EDAMAM_ID"]
-  EDAMAM_KEY = ENV["EDAMAM_KEY"]
-
+class RecipesController < ApplicationController
   def index
+    # @recipes = EdamamApiWrapper.random
   end
 
-  # list of recipes that are returned from a user
-  def self.search(terms)
-    query_params = {
-      "q" => terms,
-      "id" => EDAMAM_ID,
-      "app_key" => EDAMAM_KEY,
-      "from" => 0,
-      "to" => 0, # first recipe
-      "health" => "dairy-free",
-    }
-    results = HTTParty.get(BASE_URL, query: query_params)
-    return results["hits"]
+  def search
+    @recipes = EdamamApiWrapper.find_recipes(params[:q])
   end
 end
-
-a = RecipesController.search("beets")
-ap a
