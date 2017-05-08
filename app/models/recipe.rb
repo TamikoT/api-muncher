3040c82792aa1ad32d6d86d5bcedc83b25b097c3
Tamiko Terada <tamikodevo@gmail.com>
2017-05-08 14:12:21 -0700

    move recipe.rb to a model dir

---
+++ app/models/recipe.rb
@@ -0,0 +1,15 @@
+class Recipe
+  attr_reader :uri, :label, :url, :image, :ingredients, :dietary, :yield, :source, :calories
+
+  def initialize(recipe)
+    @uri = recipe["recipe"]["uri"]
+    @label = recipe["recipe"]["label"]
+    @url = recipe["recipe"]["shareAs"]
+    @image = recipe["recipe"]["image"]
+    @ingredients = recipe["recipe"]["ingredientLines"]
+    @dietary = recipe["recipe"]["healthLabels"]
+    @yield = recipe["recipe"]["yield"]
+    @source = recipe["recipe"]["source"]
+    @calories = recipe["recipe"]["calories"].to_i
+  end
+end
