508c5b0a7601ef31597c7864e95c72b95cf5da8b
Tamiko Terada <tamikodevo@gmail.com>
2017-05-09 01:18:52 -0700

    create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'recipes#index'
+  root 'recipes#home'
   get '/recipes/search', to: 'recipes#search', as: 'search'
   get '/recipes', to: 'recipes#index', as: 'recipes'
+  get '/recipes/:page', to: 'recipes#index'
   get '/recipes/:index', to: 'recipes#show', as: 'recipe'
 end
