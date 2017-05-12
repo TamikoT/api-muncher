9b0b7e02ad8f428b3a2ef6d48b7f9d135a01cec6
Tamiko Terada <tamikodevo@gmail.com>
2017-05-11 22:42:27 -0700

    add recipe migration back in

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504164518) do
+ActiveRecord::Schema.define(version: 20170509094219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
